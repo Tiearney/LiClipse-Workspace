b651dba295676824f6dd32aaca54eea58277f991
Tiearney <96931364+Tiearney@users.noreply.github.com>
2021-12-31 23:21:23 -0600

    Add files via upload

---
+++ variablesOutline.py
@@ -0,0 +1,108 @@
+'''
+This outline will help solidify concepts from the Variables lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1)
+#Make an example variable of each of the types of variables (int, float, 
+#string, boolean and list):
+
+#Integer
+
+a = 3
+print (a)
+
+a = 2
+print (a)
+
+a = 1
+print (a)
+
+#Float
+
+a = 3.45
+
+a = 5.43
+
+
+a = 4.32
+ 
+#String
+
+a = "Samantha"
+
+a = "Michael"
+
+a = "Adrien"
+
+a = "Samantha and Michael"
+
+#Boolean
+
+a = True
+
+a = False
+
+#List
+
+a = [1, 2,3, 4]
+
+a = ["Sam", "Michael", "Adrien"]
+
+a = [True, False, True]
+
+a = [3.2, 4.5, 5.8]
+
+print (a)
+
+#2)
+#Make two variables one named 'a' and one named 'A' both set to  different data:
+#(Remember that capitalization matters, so these can be two different variables)
+
+varA = 1
+vara = 2
+
+var = 4
+var = "string"
+
+#3)
+#Make a variable, and then go down a couple lines and change the data in that
+#same variable:
+
+varTwo = var
+print(varTwo)
+
+#4)
+#Make a variable and set it to a variable you have already created:
+
+b = [a, varA, var]
+print(b)
+
+#5)
+#Make a list with three items. Make a variable that is set to the second
+#item in that list:
+
+list = ["a", "b", "c"]
+print(list[2])
+
+listVar = list[0]
+print(listVar)
+
+#6)
+#Make a String set to your name. Make a variable that is set to the first
+#character in that string:
+
+string = "Samantha"
+print(string[0])
+
+#7)
+#Print out ONE of any of the variables you have created so far:
+
+print("string")
+
+#8)
+#Make a variable of any type, with one of the common SYNTAX errors
+
+var == 4 
+
+4 = var
