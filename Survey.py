a42d858d31aa3bad152799e377b0825971067db6
Tiearney <96931364+Tiearney@users.noreply.github.com>
2022-01-02 01:04:33 -0600

    Add files via upload

---
+++ Survey.py
@@ -0,0 +1,31 @@
+'''
+This project is a survey
+
+@author: Tiearney Beltz
+
+Created on Jan 2, 2022
+'''
+
+'''
+The program will be taking in information from 
+the user's answers to the questions
+'''
+'''
+The user will be asked five different questions 
+and will answer them truthfully
+'''
+
+name = input("What is your name?")
+print(name)
+
+color = input("What is your favorite color?")
+print(color)
+
+city = input("What city did you grow up in?")
+print(city)
+
+friend = input("What is your best friends name?")
+print(friend)
+
+hair = input("What color hair do you have?")
+print(hair)
