e265b68150f474d9bdf7b5bfd0bef742712c6a63
Tiearney <96931364+Tiearney@users.noreply.github.com>
2022-01-01 23:02:46 -0600

    Add files via upload

---
+++ commentDrills.py
@@ -0,0 +1,42 @@
+'''
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a single line comment
+
+#This is a single line comment
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Make a single line comment.
+
+#I am going to the grocery store
+
+#2) Make a multi-line comment.
+
+'''
+I 
+don't
+like 
+peanut
+butter
+'''
+
+#3) Make a variable. Then put a comment after it, on the same line.
+
+var = 3 #Three is my favorite number
+
+#4) Make a single line comment.
+
+#Snow is cool
+
+#5) Make a multi-line comment.
+
+'''My favorite season is summer'''
