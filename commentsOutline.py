f46bb23dd6d230ddce1eed0eb6450f982a880e75
Tiearney <96931364+Tiearney@users.noreply.github.com>
2022-01-01 22:52:45 -0600

    Add files via upload

---
+++ commentsOutline.py
@@ -0,0 +1,40 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+from email._header_value_parser import Comment
+from test.test_typing import CoolEmployee
+
+#1) Make a single line comment.
+
+#This is a single line comment
+
+#I am a comment
+
+#2) Make a multi-line comment.
+
+'''
+This
+is
+a
+multi-line
+comment
+'''
+
+'''
+I am a multi-line comment
+because I have more than one line
+'''
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+
+var = 2 #This is an integer variable
+
+'''Syntax errors and logical errors'''
+
+i am Comment
+
+'''i am comment
+my name is samantha 
+this is cool 
