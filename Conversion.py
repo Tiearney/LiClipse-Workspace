10c8c715b93e32357a725bf11451ff6ed63d6391
Tiearney <96931364+Tiearney@users.noreply.github.com>
2022-02-21 18:44:48 -0600

    Add files via upload
    2-21-22

---
+++ Conversion.py
@@ -0,0 +1,51 @@
+'''
+Created on Feb 20, 2022
+
+@author: ITAUser
+'''
+'''Tiearney Beltz'''
+
+'''The objective is to convert the number of miles to yards, feet, and inches'''
+
+#Use input() to get the number of miles from the user. And store
+#that int in a variable called miles.
+miles = 1
+print(miles)
+
+yards = 1760
+print(yards)
+
+feet = 5280
+print(feet)
+
+inch = 63360
+print(inch)
+#Convert miles to yards, using the following:
+# 1 mile = 1760 yards.
+
+print(str(miles) + " miles converts to " + str(yards) + " yards ")
+
+#Store the value in a variable called yards and print it out with a 
+#simple statement.
+
+print(" This is how many yards are in a mile : " + str(yards) + " yd ")
+
+#Convert miles to feet, using the following:
+# 1 mile = 5280 feet.
+
+print(str(miles) + " miles converts to " + str(feet) + " feet ")
+
+#Store the value in a variable called feet and print it out with a 
+#simple statement.
+
+print(" This is how my feet are in a mile : " + str(feet) + " ft ")
+
+#Convert miles to inches, using the following:
+# 1 mile = 63,360 inches.
+
+print(str(miles) + " miles converts to " + str(inch) + " inches ")
+
+#Store the value in a variable called inches and print it out with a 
+#simple statement.
+
+print(" This is how many inches are in a mile : " + str(inch) + " in ")
