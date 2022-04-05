cb60774bf35c83f0c85533d777434436d86ec4cd
Tiearney <96931364+Tiearney@users.noreply.github.com>
2022-04-04 19:05:04 -0500

    Add files via upload

---
+++ conditionalOutline.py
@@ -0,0 +1,47 @@
+'''
+This outline will help solidify concepts from the Conditionals lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a int variable named a. Now make an if statement, and if a is more than
+#5, print out "a is more than 5."
+
+a = 7
+
+if a>5:
+    print("a is more than 5")
+
+#2) Make a boolean variable named b. Now make an if statement, and if b is True
+#print out "b is True."
+
+b = 8
+if b==8: 
+    print("b is True")
+
+#3) Make an int variable named c. Now make an if/elif statement, and if c is 
+#more than 0, print "c is positive". Else if c is less than 0, print "c is
+#negative".
+
+c = 12
+if c >= 12: 
+    print("c is positive")
+
+else:
+    print("c is negative")
+
+#4) Make an int variable named d. Now make an if/elif/else statement, and if 
+#d is more than 0, print "d is positive". Else if d is less than 0, print "c is
+#negative". Else, print "d is zero".
+
+d = -10
+
+if d <= -10:
+    print("d is positive")
+    
+else: 
+    print("d is negative")
+
+#5) Make an if statement with any comparison, with a common SYNTAX error.
+
+#missing the if/elif/else
+
